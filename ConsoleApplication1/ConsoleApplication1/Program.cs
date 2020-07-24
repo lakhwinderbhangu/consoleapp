9ad6f5d42f04dd2fe0a2f954e592bd10b996e639
lalli bhangulalaadasdfsf <lakhwinder.bhangu@gmail.com>
2020-07-24 12:49:18 -0600

    i added newclass.cs and change in program.cs today
    some description

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("first change");
+            Console.WriteLine("second change");
         }
     }
 }
